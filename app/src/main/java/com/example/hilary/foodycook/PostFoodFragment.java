fcfbb5df4426d944e1f6380d37f126a9f8e17950
hilpitome <hilpitome@gmail.com>
2017-04-28 11:05:01 +0300

    post food fragment

--- app/src/main/java/com/example/hilary/foodycook/PostFoodFragment.java
+++ app/src/main/java/com/example/hilary/foodycook/PostFoodFragment.java
@@ -113,29 +113,12 @@ public class PostFoodFragment extends Fragment{
 
         //chakulaImage.setImageDrawable(Drawable.createFromPath(file.getAbsolutePath()));
         if(file.exists()){
-            int targetW = chakulaImage.getWidth();
-            int targetH = chakulaImage.getHeight();
 
-            // Get the dimensions of the bitmap
-            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-            bmOptions.inJustDecodeBounds = true;
-            BitmapFactory.decodeFile(file.getAbsolutePath(), bmOptions);
-            int photoW = bmOptions.outWidth;
-            int photoH = bmOptions.outHeight;
-
-            // Determine how much to scale down the image
-            int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
-
-            // Decode the image file into a Bitmap sized to fill the View
-            bmOptions.inJustDecodeBounds = false;
-            bmOptions.inSampleSize = scaleFactor;
-            bmOptions.inPurgeable = true;
-            Bitmap bMap = BitmapFactory.decodeFile(file.getAbsolutePath(), bmOptions);
+            Bitmap bMap = BitmapFactory.decodeFile(file.getAbsolutePath());
 
             chakulaImage.setImageBitmap(bMap);
 
             photoButton.setVisibility(View.GONE);
-            postItem.setVisibility(View.VISIBLE);
 
         }
 
